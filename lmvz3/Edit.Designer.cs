9f00fd2b313d43e9908a38dab1c55c9564be8779
kostemkin@gmail.com <kostemkin@gmail.com>
2014-11-26 01:20:31 +0200

    FormEdit changes

--- lmvz3/Edit.Designer.cs
+++ lmvz3/Edit.Designer.cs
@@ -28,33 +28,235 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.fio = new System.Windows.Forms.Label();
+            this.id = new System.Windows.Forms.Label();
+            this.pass = new System.Windows.Forms.Label();
+            this.birth = new System.Windows.Forms.Label();
+            this.faculty = new System.Windows.Forms.Label();
+            this.group = new System.Windows.Forms.Label();
+            this.formStudy = new System.Windows.Forms.Label();
+            this.number = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // fio
             // 
-            this.button1.Location = new System.Drawing.Point(47, 61);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Edit";
-            this.button1.UseVisualStyleBackColor = true;
+            this.fio.AutoSize = true;
+            this.fio.Location = new System.Drawing.Point(12, 29);
+            this.fio.Name = "fio";
+            this.fio.Size = new System.Drawing.Size(34, 13);
+            this.fio.TabIndex = 0;
+            this.fio.Text = "ФИО";
+            // 
+            // id
+            // 
+            this.id.AutoSize = true;
+            this.id.Location = new System.Drawing.Point(12, 85);
+            this.id.Name = "id";
+            this.id.Size = new System.Drawing.Size(149, 13);
+            this.id.TabIndex = 1;
+            this.id.Text = "Идентификационный номер";
+            // 
+            // pass
+            // 
+            this.pass.AutoSize = true;
+            this.pass.Location = new System.Drawing.Point(12, 141);
+            this.pass.Name = "pass";
+            this.pass.Size = new System.Drawing.Size(91, 13);
+            this.pass.TabIndex = 2;
+            this.pass.Text = "Номер паспорта";
+            // 
+            // birth
+            // 
+            this.birth.AutoSize = true;
+            this.birth.Location = new System.Drawing.Point(12, 195);
+            this.birth.Name = "birth";
+            this.birth.Size = new System.Drawing.Size(86, 13);
+            this.birth.TabIndex = 3;
+            this.birth.Text = "Дата рождения";
+            // 
+            // faculty
+            // 
+            this.faculty.AutoSize = true;
+            this.faculty.Location = new System.Drawing.Point(12, 253);
+            this.faculty.Name = "faculty";
+            this.faculty.Size = new System.Drawing.Size(63, 13);
+            this.faculty.TabIndex = 4;
+            this.faculty.Text = "Факультет";
+            // 
+            // group
+            // 
+            this.group.AutoSize = true;
+            this.group.Location = new System.Drawing.Point(12, 310);
+            this.group.Name = "group";
+            this.group.Size = new System.Drawing.Size(42, 13);
+            this.group.TabIndex = 5;
+            this.group.Text = "Группа";
+            // 
+            // formStudy
+            // 
+            this.formStudy.AutoSize = true;
+            this.formStudy.Location = new System.Drawing.Point(12, 362);
+            this.formStudy.Name = "formStudy";
+            this.formStudy.Size = new System.Drawing.Size(93, 13);
+            this.formStudy.TabIndex = 6;
+            this.formStudy.Text = "Форма обучения";
+            // 
+            // number
+            // 
+            this.number.AutoSize = true;
+            this.number.Location = new System.Drawing.Point(12, 415);
+            this.number.Name = "number";
+            this.number.Size = new System.Drawing.Size(93, 13);
+            this.number.TabIndex = 7;
+            this.number.Text = "Номер телефона";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(59, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Режим просмотра";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 45);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(290, 20);
+            this.textBox1.TabIndex = 9;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(15, 101);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(290, 20);
+            this.textBox2.TabIndex = 10;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(15, 157);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(125, 20);
+            this.textBox3.TabIndex = 11;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(15, 211);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 13;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(15, 269);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 14;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(15, 326);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 15;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(15, 378);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 21);
+            this.comboBox3.TabIndex = 16;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(15, 431);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(163, 20);
+            this.textBox4.TabIndex = 17;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.BackColor = System.Drawing.SystemColors.Control;
+            this.trackBar1.LargeChange = 2;
+            this.trackBar1.Location = new System.Drawing.Point(242, 9);
+            this.trackBar1.Maximum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.trackBar1.RightToLeftLayout = true;
+            this.trackBar1.Size = new System.Drawing.Size(63, 45);
+            this.trackBar1.SmallChange = 2;
+            this.trackBar1.TabIndex = 18;
+            this.trackBar1.TickFrequency = 2;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
             // 
             // Edit
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(230, 262);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(317, 471);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.number);
+            this.Controls.Add(this.formStudy);
+            this.Controls.Add(this.group);
+            this.Controls.Add(this.faculty);
+            this.Controls.Add(this.birth);
+            this.Controls.Add(this.pass);
+            this.Controls.Add(this.id);
+            this.Controls.Add(this.fio);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Edit";
             this.Text = "Edit";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label fio;
+        private System.Windows.Forms.Label id;
+        private System.Windows.Forms.Label pass;
+        private System.Windows.Forms.Label birth;
+        private System.Windows.Forms.Label faculty;
+        private System.Windows.Forms.Label group;
+        private System.Windows.Forms.Label formStudy;
+        private System.Windows.Forms.Label number;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TrackBar trackBar1;
+
     }
 }
--- lmvz3/Edit.cs
+++ lmvz3/Edit.cs
@@ -12,9 +12,40 @@ namespace lmvz3
 {
     public partial class Edit : Form
     {
+        List<Control> textboxes = new List<Control>();
+       
         public Edit()
         {
             InitializeComponent();
+            textboxes.Add(textBox1);
+            textboxes.Add(textBox2);
+            textboxes.Add(textBox3);
+            textboxes.Add(textBox4);
+            textboxes.Add(dateTimePicker1);
+            textboxes.Add(comboBox1);
+            textboxes.Add(comboBox2);
+            textboxes.Add(comboBox3);
+            foreach (Control controls in textboxes)
+                controls.Enabled = false;
+            textBox1.BringToFront();
         }
-    }
+          
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            if (trackBar1.Value == 0)
+            {
+                label1.Text = "Режим просмотра";
+                foreach (Control controls in textboxes)
+                    controls.Enabled = false;
+            }
+            else
+            {
+                label1.Text = "Режим редактирования";
+                foreach (Control controls in textboxes)
+                    controls.Enabled = true;
+            }
+        }
+
+  }
 }
